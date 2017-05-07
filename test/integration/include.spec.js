f93eb954b6fee0630505c8ea17150f8cb827809a
Tim Schaub <tim.schaub@gmail.com>
2017-05-07 15:55:43 -0600

    Integration tests

---
+++ test/integration/include.spec.js
@@ -0,0 +1,40 @@
+var helper = require('../helper');
+var ghPages = require('../../lib/');
+var path = require('path');
+
+var fixtures = path.join(__dirname, 'fixtures');
+var fixtureName = 'include';
+
+beforeEach(function() {
+  ghPages.clean();
+});
+
+describe('the src option', function() {
+  it('can be used to limit which files are included', function(done) {
+    var local = path.join(fixtures, fixtureName, 'local');
+    var branch = 'gh-pages';
+
+    helper.setupRemote(fixtureName, branch).then(function(url) {
+      var options = {
+        repo: url,
+        src: '**/*.js',
+        user: {
+          name: 'User Name',
+          email: 'user@email.com'
+        }
+      };
+
+      ghPages.publish(local, options, function(err) {
+        if (err) {
+          return done(err);
+        }
+        helper
+          .assertContentsMatch(local, url, branch, {includeFilter: options.src})
+          .then(function() {
+            done();
+          })
+          .catch(done);
+      });
+    });
+  });
+});
