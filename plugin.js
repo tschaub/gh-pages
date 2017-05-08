aa49b8a01eef5a884b6d8ebedf3410f39b138506
Tim Schaub <tim.schaub@gmail.com>
2017-05-07 23:26:24 -0600

    Lint all js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
-var ghPages = require('./lib/index')
-var path = require('path')
+var ghPages = require('./lib/index');
+var path = require('path');
 
-module.exports = function (pluginConfig, config, callback) {
-  ghPages.publish(path.join(process.cwd(), config.basePath), config, callback)
-}
+module.exports = function(pluginConfig, config, callback) {
+  ghPages.publish(path.join(process.cwd(), config.basePath), config, callback);
+};
