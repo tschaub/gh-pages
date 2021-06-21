6ea427b5576b96f594f3adc4da8bf527c3866fd0
Tim Schaub <tim.schaub@gmail.com>
2021-06-21 14:47:12 -0600

    Test cache dir path creation

---
+++ test/lib/index.spec.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const assert = require('../helper').assert;
+const index = require('../../lib/index');
+
+describe('index', () => {
+  describe('getCacheDir()', () => {
+    it('works for git@github.com:<username>/<project>.git', () => {
+      const dir = index.getCacheDir(
+        'git@github.com:example-user/example-project.git'
+      );
+
+      const expected = path.join(
+        '.cache',
+        'gh-pages',
+        'git@github.com!example-user!example-project.git'
+      );
+      assert(dir.endsWith(expected), `unexpected cache dir: ${dir}`);
+    });
+
+    it('works for https://github.com/<username>/<project>.git', () => {
+      const dir = index.getCacheDir(
+        'https://github.com/example-user/example-project.git'
+      );
+
+      const expected = path.join(
+        '.cache',
+        'gh-pages',
+        'https!github.com!example-user!example-project.git'
+      );
+      assert(dir.endsWith(expected), `unexpected cache dir: ${dir}`);
+    });
+
+    it('works for https://<username>:<password>@github.com/<username>/<project>.git', () => {
+      const dir = index.getCacheDir(
+        'https://user:pass@github.com/example-user/example-project.git'
+      );
+
+      const expected = path.join(
+        '.cache',
+        'gh-pages',
+        'https!user!pass@github.com!example-user!example-project.git'
+      );
+      assert(dir.endsWith(expected), `unexpected cache dir: ${dir}`);
+    });
+  });
+});
