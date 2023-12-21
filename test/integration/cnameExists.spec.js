b6b84541cb7918ce1b99ea9688808f63af0c356a
WillBAnders <WillBAnders@gmail.com>
2023-12-20 22:25:57 -0500

    Add test for cnameExists, asserting it replaces the existing CNAME

---
+++ test/integration/cnameExists.spec.js
@@ -0,0 +1,38 @@
+const helper = require('../helper.js');
+const ghPages = require('../../lib/index.js');
+const path = require('path');
+
+const fixtures = path.join(__dirname, 'fixtures');
+const fixtureName = 'cname-exists';
+
+beforeEach(() => {
+  ghPages.clean();
+});
+
+describe('the --cname option', () => {
+  it('works even if the CNAME file already exists AND replaces any existing value', (done) => {
+    const local = path.join(fixtures, fixtureName, 'local');
+    const expected = path.join(fixtures, fixtureName, 'expected');
+    const branch = 'gh-pages';
+
+    helper.setupRemote(fixtureName, {branch}).then((url) => {
+      const options = {
+        repo: url,
+        user: {
+          name: 'User Name',
+          email: 'user@email.com',
+        },
+        cname: 'custom-domain.com',
+      };
+      ghPages.publish(local, options, (err) => {
+        if (err) {
+          return done(err);
+        }
+        helper
+          .assertContentsMatch(expected, url, branch)
+          .then(() => done())
+          .catch(done);
+      });
+    });
+  });
+});
